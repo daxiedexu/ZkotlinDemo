16a6a6dcc993b9aaae31f59eb95188e6e7216b49
zhanghaixu <3070448724@qq.com>
2023-05-01 17:44:10 +0800

    feat(other)：实现四则运算逻辑

--- app/src/main/java/com/haixu/zkotilndemo/MainActivity.kt
+++ app/src/main/java/com/haixu/zkotilndemo/MainActivity.kt
@@ -2,10 +2,48 @@ package com.haixu.zkotilndemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        cale("3","-","3")
+    }
+
+    /**
+     * 四则运算
+     */
+    private fun cale(vararg args: String){
+        if (args.size != 3){
+            return showHelp()
+        }
+
+        val operator = mapOf(
+            "+" to ::add,
+            "-" to ::del
+        )
+        
+        val op = args[1]
+        val opFunc = operator[op] ?: return showHelp()
+        Log.d(TAG, "input${args.joinToString(",")}")
+        Log.d(TAG, "result ${opFunc(args[0].toInt(), args[2].toInt())}")
+    }
+
+    private fun add(p1: Int, p2: Int): Int{
+        return p1 + p2
+    }
+
+    private fun del(p1: Int, p2: Int): Int{
+        return p1 - p2
+    }
+
+    private fun showHelp(){
+        Log.d(TAG, """
+            input size should is 3
+            demo input value is 4*5
+            """.trimIndent()
+        )
     }
 }
